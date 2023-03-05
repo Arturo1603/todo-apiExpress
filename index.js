600f36e27a2a0bc8c2e9cdda0b4beb141f1dd8d0
Arturo <arturo.cq.16@gmail.com>
2023-03-04 19:57:45 -0500

    add crud todo-api

--- index.js
+++ index.js
@@ -5,13 +5,52 @@ import express from "express";
 const app = express();
 
 
+app.use(express.json());
+
+const todoListArray = [];
+
 app.get("/", (request, response) => {
     response.json({
-        message: "Hola mundo",
+        total: todoListArray.length,
+        tasks: todoListArray,
     })
 
 });
 
+app.post("/create-task", (request, response) => {
+    const task = request.body;
+    task.id = todoListArray.length + 1;
+    task.created_at = new Date();
+
+    todoListArray.push(task);
+
+    response.json({
+        message: "Task Created",
+        task
+    });
+});
+
+
+app.put("/update-task/:id", (request, response) => {
+    const id = request.params.id;
+    const data = request.body;
+
+    const taskIndex = todoListArray.findIndex((todotask) => todotask.id == Number(id));
+
+    todoListArray[taskIndex] = { ...todoListArray[taskIndex], ...data };
+
+    return response.json({
+        tarea: "update Task",
+    });
+});
+
+app.delete("/delete-task/:id", (request, response)=> {
+    const id = request.params.id;
+    todoListArray.splice(id - 1, 1);
+    response.json({
+        message: "task "+id+" was eliminated susccesfully"
+    });
+});
 
 
 app.listen(6001, () => {
